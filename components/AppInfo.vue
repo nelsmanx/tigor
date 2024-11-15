5d67c8f146286676dac47f3d59323ea870f7d25e
nelsmanx <nelsmanx@example.com>
2024-11-15 15:24:15 +0300

    AppInfo edits

--- components/AppInfo.vue
+++ components/AppInfo.vue
@@ -4,9 +4,11 @@
 			<div class="info__inner">
 				<div class="info__block-1">
 					<h2 class="info__title">Аутсорсинг персонала в&nbsp;Москве и&nbsp;Московской области</h2>
-					<img class="info__image" src="/images/deliverymen.png" alt="" aria-hidden="true">
-					<a class="info__link" href="#">Узнать подробнее</a>
-					<a class="info__mail" href="mailto:info@tigor.pro">info@tigor.pro</a>
+					<div class="info__group">
+						<img class="info__image" src="/images/deliverymen.png" alt="" aria-hidden="true">
+						<a class="info__link" href="#">Узнать подробнее</a>
+						<a class="info__mail" href="mailto:info@tigor.pro">info@tigor.pro</a>
+					</div>
 				</div>
 				<div class="info__block-2">
 					<div class="info__desc">
@@ -49,12 +51,22 @@
 	color: var(--font-basic);
 }
 
+.info__group {
+	display: grid;
+	grid-template-areas:
+		"image"
+		"link"
+		"mail";
+}
+
 .info__image {
+	grid-area: image;
 	width: 254px;
 	margin-bottom: 60px;
 }
 
 .info__link {
+	grid-area: link;
 	display: block;
 	width: fit-content;
 	margin-bottom: 15px;
@@ -63,7 +75,7 @@
 	font-weight: 400;
 	color: var(--font-basic);
 	background-size: 18px 10px;
-	background-position: right bottom 9px;
+	background-position: right bottom 7px;
 	background-repeat: no-repeat;
 	background-image: url("data:image/svg+xml,%3Csvg width='18' height='10' viewBox='0 0 18 10' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M17.4304 4.38078L13.2582 0.208609C12.9801 -0.0695362 12.4238 -0.0695362 12.1457 0.208609C11.8675 0.486754 11.8675 1.04304 12.1457 1.32119L14.9271 4.10264H0.834435C0.37086 4.10264 0 4.4735 0 4.93707C0 5.40065 0.37086 5.77151 0.834435 5.77151H14.9271L12.1457 8.55296C11.8675 8.8311 11.8675 9.38739 12.1457 9.66554C12.4238 9.94368 12.9801 9.94368 13.2582 9.66554L17.4304 5.49336C17.7086 5.21522 17.7086 4.65893 17.4304 4.38078Z' fill='%231C1C1C' /%3E%3C/svg%3E");
 }
@@ -73,6 +85,7 @@ html[data-color-scheme="dark"] .info__link {
 }
 
 .info__mail {
+	grid-area: mail;
 	display: block;
 	width: fit-content;
 	font-weight: 500;
@@ -105,10 +118,29 @@ html[data-color-scheme="dark"] .info__link {
 @media (max-width: 1399.98px) {
 	.info__inner {
 		grid-template-columns: 100%;
+		gap: 80px;
 	}
 
 	.info__title {
 		max-width: none;
+	}
+
+	.info__group {
+		grid-template-areas:
+			"image link"
+			"image mail";
+		column-gap: 40px;
+		row-gap: 10px;
+		width: fit-content;
+	}
+
+	.info__image {
+		margin-bottom: 0;
+	}
+
+	.info__link {
+		margin-bottom: 0;
+		align-self: end;
 	}
 
 	.info__desc-par:not(:last-child) {
@@ -121,9 +153,12 @@ html[data-color-scheme="dark"] .info__link {
 		padding: var(--section-space) 0;
 	}
 
+	.info__inner {
+		gap: 60px;
+	}
+
 	.info__image {
 		width: 200px;
-		margin-bottom: 40px;
 	}
 
 	.info__desc-par {
@@ -135,22 +170,27 @@ html[data-color-scheme="dark"] .info__link {
 	}
 
 	.info__link {
-		margin-bottom: 10px;
 		padding-right: 28px;
 		font-size: 20px;
 		background-size: 16px 9px;
-		background-position: right bottom 7px;
 	}
 }
 
 @media (max-width: 767.98px) {
+	.info__inner {
+		gap: 50px;
+	}
+
 	.info__title {
 		margin-bottom: 30px;
 	}
 
+	.info__group {
+		column-gap: 30px;
+	}
+
 	.info__image {
 		width: 180px;
-		margin-bottom: 30px;
 	}
 
 	.info__desc-par {
@@ -167,37 +207,66 @@ html[data-color-scheme="dark"] .info__link {
 		background-size: 14px 8px;
 		background-position: right bottom 6px;
 	}
+}
 
-	@media (max-width: 575.98px) {
-		.info__image {
-			width: 160px;
-			margin-bottom: 20px;
-		}
-
-		.info__link {
-			margin-bottom: 6px;
-			padding-right: 18px;
-			font-size: 16px;
-			background-size: 12px 7px;
-		}
-
-		.info__desc-par {
-			font-size: 16px;
-		}
-
-		.info__desc-par:not(:last-child) {
-			margin-bottom: 12px;
-		}
+@media (max-width: 575.98px) {
+	.info__inner {
+		gap: 40px;
 	}
 
-	@media (max-width: 479.98px) {
-		.info__desc-par {
-			font-size: 14px;
-		}
+	.info__image {
+		width: 160px;
+	}
 
-		.info__desc-par:not(:last-child) {
-			margin-bottom: 10px;
-		}
+	.info__group {
+		column-gap: 25px;
+		row-gap: 6px;
+	}
+
+	.info__link {
+		padding-right: 18px;
+		font-size: 16px;
+		background-size: 12px 7px;
+	}
+
+	.info__desc-par {
+		font-size: 16px;
+	}
+
+	.info__desc-par:not(:last-child) {
+		margin-bottom: 12px;
+	}
+}
+
+@media (max-width: 479.98px) {
+	.info__desc-par {
+		font-size: 14px;
+	}
+
+	.info__desc-par:not(:last-child) {
+		margin-bottom: 10px;
+	}
+}
+
+@media (max-width: 379.98px) {
+	.info__inner {
+		gap: 30px;
+	}
+
+	.info__group {
+		grid-template-areas:
+			"image"
+			"link"
+			"mail";
+		gap: 0;
+	}
+
+	.info__image {
+		margin-bottom: 20px;
+	}
+
+	.info__link {
+		margin-bottom: 6px;
 	}
 }
 </style>
